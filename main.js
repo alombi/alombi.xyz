11a9240139c3045b55017ae2591c11c10d9c2314
alombi <alessandrolombardo@AirdiAlessandro.station>
2020-06-01 13:12:08 +0200

    Added main.js and updated the navbar

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById('navbar').style.display = 'block';
+	document.getElementById('page').style.display = 'none';
+};
+
+function closeNav() {
+    document.getElementById('navbar').style.display = 'none';
+	document.getElementById('page').style.display = 'block';
+}
