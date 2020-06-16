7990c8ec3981d035ad211a6652533211d5f369d1
alombi <alessandrolombardo@AirdiAlessandro.station>
2020-06-16 15:42:24 +0200

    Deleted most files. I'm restyling.

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+//code by @alombi. 2020 alombi
+
+//Navbar functions (open)
 function openNav() {
     document.getElementById('navbar').style.display = 'block';
 	document.getElementById('page').style.display = 'none';
 };
 
+//Navbar functions (close)
 function closeNav() {
     document.getElementById('navbar').style.display = 'none';
 	document.getElementById('page').style.display = 'block';
