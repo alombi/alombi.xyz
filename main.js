9e9ccb7659a3eec8e69d77ff030f88346bfe1c7f
alombi <alessandrolombardo@MacBook-Air-di-Alessandro.local>
2020-08-24 02:07:36 +0200

    Added partial web dev resources

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ function closeNav() {
     document.getElementById('navbar').style.display = 'none';
 	document.getElementById('page').style.display = 'block';
 }
+// table of contents
+function openTable(){
+    document.getElementById('table_of_contents').style.display = 'block';
+	document.getElementById('main').style.display = 'none';
+}
+
+function closeTable(){
+    document.getElementById('table_of_contents').style.display = 'none';
+	document.getElementById('main').style.display = 'block';
+}
