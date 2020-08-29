c6d94ad851f1c2746337a43d352aea7c3979e6eb
alombi <alessandrolombardo@MacBook-Air-di-Alessandro.local>
2020-08-29 12:04:14 +0200

    Added icons

--- main.js
+++ main.js
@@ -15,9 +15,11 @@ function closeNav() {
 function openTable(){
     document.getElementById('table_of_contents').style.display = 'block';
 	document.getElementById('main').style.display = 'none';
+	document.getElementById('topnav').style.display = 'none';
 }
 
 function closeTable(){
     document.getElementById('table_of_contents').style.display = 'none';
-	document.getElementById('main').style.display = 'block';
+    document.getElementById('main').style.display = 'block';
+    document.getElementById('topnav').style.display = 'block';
 }
