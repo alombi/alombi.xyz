122e3dab6f7b21bb2248cdea4de412d47a5e5644
alombi <alessandrolombardo@MacBook-Air-di-Alessandro.local>
2020-08-30 14:55:34 +0200

    Added a lot of stuff

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
 //code by @alombi. 2020 alombi
 
-//Navbar functions (open)
-function openNav() {
-    document.getElementById('navbar').style.display = 'block';
-	document.getElementById('page').style.display = 'none';
-};
-
-//Navbar functions (close)
-function closeNav() {
-    document.getElementById('navbar').style.display = 'none';
-	document.getElementById('page').style.display = 'block';
-}
 // table of contents
 function openTable(){
     document.getElementById('table_of_contents').style.display = 'block';
