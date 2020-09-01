dec9a54a0d628f505cee374bdd9fd0912893499e
alombi <alessandrolombardo@MacBook-Air-di-Alessandro.local>
2020-09-01 15:03:32 +0200

    Fixed resources.html

--- main.js
+++ main.js
@@ -15,37 +15,37 @@ function closeTable(){
 
 // Resources
 function hosting(){
-    document.getElementById('main').style.opacity = 0.1;
+    document.getElementById('main').style.opacity = 0.03;
     document.getElementById('hosting').style.display = 'block';
     document.getElementById('main').style.pointerEvents = 'none';
     document.getElementsByTagName("BODY")[0].style.overflowY = 'hidden';
 }
 function subdomains(){
-    document.getElementById('main').style.opacity = 0.1;
+    document.getElementById('main').style.opacity = 0.03;
     document.getElementById('subdomains').style.display = 'block';
     document.getElementById('main').style.pointerEvents = 'none';
     document.getElementsByTagName("BODY")[0].style.overflowY = 'hidden';
 }
 function editors(){
-    document.getElementById('main').style.opacity = 0.1;
+    document.getElementById('main').style.opacity = 0.03;
     document.getElementById('editors').style.display = 'block';
     document.getElementById('main').style.pointerEvents = 'none';
     document.getElementsByTagName("BODY")[0].style.overflowY = 'hidden';
 }
 function remote(){
-    document.getElementById('main').style.opacity = 0.1;
+    document.getElementById('main').style.opacity = 0.03;
     document.getElementById('remote').style.display = 'block';
     document.getElementById('main').style.pointerEvents = 'none';
     document.getElementsByTagName("BODY")[0].style.overflowY = 'hidden';
 }
 function css(){
-    document.getElementById('main').style.opacity = 0.1;
+    document.getElementById('main').style.opacity = 0.03;
     document.getElementById('css').style.display = 'block';
     document.getElementById('main').style.pointerEvents = 'none';
     document.getElementsByTagName("BODY")[0].style.overflowY = 'hidden';
 }
 function others(){
-    document.getElementById('main').style.opacity = 0.1;
+    document.getElementById('main').style.opacity = 0.03;
     document.getElementById('others').style.display = 'block';
     document.getElementById('main').style.pointerEvents = 'none';
     document.getElementsByTagName("BODY")[0].style.overflowY = 'hidden';
