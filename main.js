c4e3a07cdfe6d0f8df1557e26c6e5e97e4adb40d
alombi <alessandrolombardo@AirdiAlessandro.home>
2021-02-06 20:53:57 +0100

    Added opacity and added auto theme

--- main.js
+++ main.js
@@ -3,11 +3,22 @@
 function switchTheme(){
    const body = document.getElementsByTagName('body')[0];
    const switchButton = document.getElementById('theme');
+   const switchLink = document.getElementById('themeButton');
    // Switch
    if(body.classList.value == 'glass'){
       body.classList.remove('glass');
       switchButton.innerHTML = 'glass theme';
+      switchLink.href = '/'
    }else{
+      body.classList.add('glass');
+      switchButton.innerHTML = 'default theme';
+   }
+}
+
+function setTheme(){
+   const body = document.getElementsByTagName('body')[0];
+   const switchButton = document.getElementById('theme');
+   if(window.location.href.indexOf('#glass') != -1){
       body.classList.add('glass');
       switchButton.innerHTML = 'default theme';
    }
