43010427512572fdf89f92be4cb445098ac88b85
alombi <alessandrolombardo@MacBook-Air-di-Alessandro.local>
2021-02-18 13:58:22 +0100

    New design

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 // 2021 © alombi
 
-function switchTheme(){
+function isDarkModeEnabled() {
+   return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+}
+
+function switchThemeGlass(){
    const body = document.getElementsByTagName('body')[0];
-   const switchButton = document.getElementById('theme');
-   const switchLink = document.getElementById('themeButton');
+   const switchButton = document.getElementById('glass_theme');
+   const switchLink = document.getElementById('glass_themeButton');
    // Switch
    if(body.classList.value == 'glass'){
       body.classList.remove('glass');
@@ -12,14 +16,32 @@ function switchTheme(){
    }else{
       body.classList.add('glass');
       switchButton.innerHTML = 'default theme';
+      const switchThemeDarkLightButton = document.getElementById('dark_light_theme');
+      switchThemeDarkLightButton.style.display = 'none';
    }
 }
 
 function setTheme(){
+   if(isDarkModeEnabled()){
+      switchThemeDarkLight()
+   }
    const body = document.getElementsByTagName('body')[0];
    const switchButton = document.getElementById('theme');
    if(window.location.href.indexOf('#glass') != -1){
       body.classList.add('glass');
-      switchButton.innerHTML = 'default theme';
+      const switchThemeDarkLightButton = document.getElementById('dark_light_theme');
+      switchThemeDarkLightButton.style.display = 'none';
+   }  
+}
+
+function switchThemeDarkLight(){
+   const body = document.getElementsByTagName('body')[0];
+   const switchThemeDarkLightButton = document.getElementById('dark_light_theme');
+   if(switchThemeDarkLightButton.name == 'moon-outline'){
+      switchThemeDarkLightButton.name = 'sunny-outline';
+      body.classList.add('dark');
+   }else if(switchThemeDarkLightButton.name == 'sunny-outline'){
+      switchThemeDarkLightButton.name = 'moon-outline';
+      body.classList.remove('dark')
    }
 }
