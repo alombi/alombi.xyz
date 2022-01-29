5b89ceed5760f7dacca0d6bc9f710f42daa15212
alombi <alombi@hotmail.com>
2022-01-29 15:36:42 +0100

    Add latest changes

---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<div>
+   <div class="title">
+      <div>
+         <h1>alombi</h1>
+         <h2>Welcome to my website.</h2>
+         <!-- <div class="links">
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+         </div> -->
+      </div>
+      <img src="https://avatars.githubusercontent.com/u/62696529?v=4" alt="">
+   </div>
+   <div class="sections">
+      <div class="section">
+         <h4>English</h4>
+         <h2 class="section-title">Code</h2>
+      </div>
+      <div class="section">
+         <h4>Italian</h4>
+         <h2 class="section-title">Blog</h2>
+      </div>
+   </div>
+</div>
