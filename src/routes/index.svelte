a28257fcf26565dc0102041148b08c6eb90fef03
alombi <alombi@hotmail.com>
2022-01-30 11:38:31 +0100

    Add refreshed home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,12 +13,16 @@
    </div>
    <div class="sections">
       <div class="section">
-         <h4>English</h4>
+         <h4 class="section-h4">English</h4>
          <h2 class="section-title">Code</h2>
+         <p>Last updated project: f1-championship</p>
+         <a class="section-link" href="/code">Browse projects <i class="fas fa-chevron-right"></i></a>
       </div>
       <div class="section">
-         <h4>Italian</h4>
+         <h4 class="section-h4">Italiano</h4>
          <h2 class="section-title">Blog</h2>
+         <p>Ultimo post: Joseph Roth, Il mercante di coralli</p>
+         <a href="/blog" class="section-link">Leggi i post <i class="fas fa-chevron-right"></i></a>
       </div>
    </div>
 </div>
