5bd505382b666d92486c00a72ff3370e95fa953c
alombi <alombi@hotmail.com>
2022-01-30 19:01:57 +0100

    Add /code page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,16 @@
+<script>
+   import posts from './blog/posts.json'
+   let lastPost = posts[0]
+</script>
 <div>
    <div class="title">
       <div>
          <h1>alombi</h1>
          <h2>Welcome to my website.</h2>
-         <!-- <div class="links">
-            <a href=""></a>
-            <a href=""></a>
-            <a href=""></a>
-         </div> -->
+         <div class="links">
+            <a id="github" href="https://github.com/alombi" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+            <a id="repo" href="https://alombi.github.io/repo" target="_blank"><i class="fas fa-hammer"></i> JB Repo</a>
+         </div>
       </div>
       <img src="https://avatars.githubusercontent.com/u/62696529?v=4" alt="">
    </div>
@@ -15,13 +18,13 @@
       <div class="section">
          <h4 class="section-h4">English</h4>
          <h2 class="section-title">Code</h2>
-         <p>Last updated project: f1-championship</p>
+         <p class="section-p">Highlighted project: <a href="">Otium</a></p>
          <a class="section-link" href="/code">Browse projects <i class="fas fa-chevron-right"></i></a>
       </div>
       <div class="section">
          <h4 class="section-h4">Italiano</h4>
          <h2 class="section-title">Blog</h2>
-         <p>Ultimo post: Joseph Roth, Il mercante di coralli</p>
+         <p class="section-p">Ultimo post: <a href={lastPost.link}>{lastPost.title}</a></p>
          <a href="/blog" class="section-link">Leggi i post <i class="fas fa-chevron-right"></i></a>
       </div>
    </div>
