3e6fa9f1396ffd0f9469a0babb31eda0481df10f
alombi <alombi@hotmail.com>
2022-02-06 23:50:49 +0100

    Fix design problem

--- src/routes/blog/layout.svelte
+++ src/routes/blog/layout.svelte
@@ -25,11 +25,11 @@
 <style>
 	h4{
 		font-size: 0.8rem;
-         opacity: 0.6;
-         text-transform: uppercase;
+   	opacity: 0.6;
+      text-transform: uppercase;
 		}
 		h1{
-			margin-top:-40px;
+			margin-top:-50px;
 		}
 	.title{
 			text-align: center;
@@ -49,6 +49,16 @@
 	@media (max-width:950px) {
 		.text{
 			width: 95vw;
+		}
+	}
+	@media (max-width:550px){
+		.title{
+			flex-direction: unset;
+		}
+		h1{
+			margin-top: -15px;
+			padding-left: 1px;
+			padding-right: 1px;
 		}
 	}
 </style>
