f919d1cf68ca2120c821da4f294c03aa9d989452
alombi <alombi@hotmail.com>
2022-02-11 22:30:37 +0100

    Adds unique visitors counter

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,29 @@
 <script>
-   import "$lib/style/style.css"
+   import "$lib/style/style.css";
+   import supabase from '$lib/db';
+   import { browser } from '$app/env'
+
+   async function addUniqueVisitor(path){
+      let request = await fetch('https://api.ipify.org?format=json')
+      let json = await request.json()
+      let request_geo = await fetch('http://ip-api.com/json/' + json.ip)
+      let json_geo = await request_geo.json()
+      let location = `${json_geo.country}, ${json_geo.regionName}`;
+      const { data, error } = await supabase.from('unique_visitors').insert([{
+         ip_address:json.ip,
+         url_position:path,
+         location: location
+      }])
+      console.log(data, error)
+   }
+   if(browser){
+      let cookie = window.localStorage.getItem('unique');
+      let path = window.location.pathname
+      if(!cookie){
+         addUniqueVisitor(path)
+         window.localStorage.setItem('unique', 'visited')
+      }
+   }
 </script>
 
 <slot></slot>
