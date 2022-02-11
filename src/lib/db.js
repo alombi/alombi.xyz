f16e6419a0df7d08a743dd2fc05bff57fe65bfdc
alombi <alombi@hotmail.com>
2022-02-11 22:38:44 +0100

    Update .env config

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,18 @@
 import { createClient } from "@supabase/supabase-js";
 
+if (process.env.NODE_ENV === 'production') {
+   // For production
+   supabaseUrl = process.env.SUPABASE_URL;
+   supabaseKey = process.env.SUPABASE_ANON_KEY;
+} else {
+   // For development
+   supabaseUrl = import.meta.env.VITE_SUPABASE_URL,
+   supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+}
+
 const supabase = createClient(
-   import.meta.env.VITE_SUPABASE_URL,
-   import.meta.env.VITE_SUPABASE_ANON_KEY
+   supabaseUrl,
+   supabaseKey
 )
 
 export default supabase
