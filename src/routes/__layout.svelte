f983fbc0db3cf0989411875e7d59e2abe8c0ee92
alombi <alombi@hotmail.com>
2022-02-13 14:19:01 +0100

    Fixes small problem

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,7 @@
    async function addUniqueVisitor(path){
       let request = await fetch('https://api.ipify.org?format=json')
       let json = await request.json()
-      let request_geo = await fetch('http://ip-api.com/json/' + json.ip)
+      let request_geo = await fetch('https://ip-api.com/json/' + json.ip)
       let json_geo = await request_geo.json()
       let location = `${json_geo.country}, ${json_geo.regionName}, ${json_geo.city}`;
       const { data, error } = await supabase.from('unique_visitors').insert([{
