9c9ad99cf431e0ee9a99a232c0ba85fbec19683c
alombi <alombi@hotmail.com>
2022-02-13 11:47:01 +0100

    Create env.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$lib/env'
+
 let supabaseUrl, supabaseKey;
 
-if (process.env.NODE_ENV === 'development') {
-   // For development
-   supabaseUrl = import.meta.env.VITE_SUPABASE_URL,
-   supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-} else {
+if (process.env.NODE_ENV === 'production') {
    // For production
    supabaseUrl = process.env.SUPABASE_URL;
    supabaseKey = process.env.SUPABASE_ANON_KEY;
+} else {
+   // For development
+   supabaseUrl = SUPABASE_URL;
+   supabaseKey = SUPABASE_ANON_KEY;
 }
 
 const supabase = createClient(
