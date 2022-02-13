675440689de05d6bb4fde25790623b8b7a29a556
alombi <alombi@hotmail.com>
2022-02-13 11:36:18 +0100

    Small improvements

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 let supabaseUrl, supabaseKey;
 
-if (process.env.NODE_ENV === 'production') {
-   // For production
-   supabaseUrl = process.env.SUPABASE_URL;
-   supabaseKey = process.env.SUPABASE_ANON_KEY;
-} else {
+if (process.env.NODE_ENV === 'development') {
    // For development
    supabaseUrl = import.meta.env.VITE_SUPABASE_URL,
    supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+} else {
+   // For production
+   supabaseUrl = process.env.SUPABASE_URL;
+   supabaseKey = process.env.SUPABASE_ANON_KEY;
 }
 
 const supabase = createClient(
