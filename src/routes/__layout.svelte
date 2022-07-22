831b82320e7732289ef11e6004ed7df3b0c5a0d4
alombi <alombi@hotmail.com>
2022-07-22 11:03:58 +0200

    Remove DB

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,40 +1,5 @@
-<script context="module">
-   import supabase from '$lib/db'
-   export async function load(){
-      const { data, error } = await supabase.from('unique_visitors').select('ip_address')
-
-      let ip_request = await fetch('https://api.ipify.org?format=json')
-      let ip_json = await ip_request.json()
-
-      return {props:{data, ip_json}}
-   }
-</script>
-
 <script>
    import "$lib/style/style.css";
-   import { browser } from '$app/env';
-   export let data, ip_json
-
-   async function updateDB(){
-      let request = await fetch('../api/' + ip_json.ip + '_unique_visitors')
-      let res = await request.json()
-   }
-
-   if(browser){
-      let cookie = window.localStorage.getItem('unique');
-      if(!cookie){
-         let alreadySent = false;
-         data.forEach(address => {
-            if(address.ip_address == ip_json.ip){
-               alreadySent = true
-            }
-         });
-         if(!alreadySent){
-            updateDB()
-            window.localStorage.setItem('unique', 'visited')
-         }
-      }
-   }
 </script>
 
 <slot></slot>
--- src/routes/blog/layout.svelte
+++ src/routes/blog/layout.svelte
@@ -5,22 +5,6 @@
 	export let date
 	export let id
 	//
-	import { browser } from '$app/env';
-
-	async function updateDB(){
-		let ip_request = await fetch('https://api.ipify.org?format=json')
-      let ip_json = await ip_request.json()
-      let request = await fetch('/api/' + ip_json.ip + '_visits_' + id)
-      let res = await request.json()
-   }
-	
-	if(browser){
-		let alreadySent = window.sessionStorage.getItem(id);
-		if(!alreadySent){
-			updateDB()
-			window.sessionStorage.setItem(id, 'visited')
-		}
-	}
 </script>
 
 <svelte:head>
