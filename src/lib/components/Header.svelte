8268f17ee8a726e30241349bd5dc5e095951d7e6
alombi <alombi@hotmail.com>
2023-01-10 12:02:42 +0100

    Add light mode

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -29,13 +29,18 @@
 
 
 <style>
+@media (prefers-color-scheme: dark){
+    .fake-nav{
+        background-color: #1c1c1c;
+    }
+}
 .fake-nav{
    position: fixed;
    top:0;
    left:0;
    width: 100%;
    height:50px;
-   background-color: #1c1c1c;
+   background-color: white;
    opacity: .6;
    z-index:3;
 }
